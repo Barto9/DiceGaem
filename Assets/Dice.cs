a369ae215efd35faff98f520336cbe3d7c674c33
Barto9 <62943350+Barto9@users.noreply.github.com>
2025-04-07 17:29:54 +0200

    Update Dice.cs

--- Assets/Dice.cs
+++ Assets/Dice.cs
@@ -8,10 +8,14 @@ public class Dice : MonoBehaviour
     public Sprite[] diceFaces;
     public SpriteRenderer sprite;
     public int currentValue;    // Stores the current dice value
-
+    public bool locked = false;
     public void RollDice()
     {
         currentValue = Random.Range(1, 6);
         sprite.sprite = diceFaces[currentValue - 1];
     }
+    public void Lock()
+    { locked = true;}
+    public void Unlock()
+    { locked = false;}
 }
